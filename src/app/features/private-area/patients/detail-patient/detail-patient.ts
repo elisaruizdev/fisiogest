1b637c087f46b84177f843db1424abd00156d8c7
Elisa Ruiz <elisaruizdev@gmail.com>
2025-11-10 20:50:26 +0100

    added card-info in detail patient

--- src/app/features/private-area/patients/detail-patient/detail-patient.ts
+++ src/app/features/private-area/patients/detail-patient/detail-patient.ts
@@ -3,16 +3,27 @@ import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { Modal } from '../../../../shared/ui/modal/modal';
 import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
+import { CardInfo } from '../../../../shared/ui/card-info/card-info';
 
 @Component({
   selector: 'app-detail-patient',
-  imports: [Modal, CommonModule, MatButtonModule],
+  imports: [Modal, CommonModule, MatButtonModule, CardInfo],
   templateUrl: './detail-patient.html',
   styleUrl: './detail-patient.scss',
 })
 export class DetailPatient {
   paciente = inject(MAT_DIALOG_DATA);
   dialogRef = inject(MatDialogRef<DetailPatient>);
+
+  pacientesData = [
+    { label: 'Nombre', value: this.paciente.nombre },
+    { label: 'Edad', value: this.paciente.edad },
+    { label: 'Género', value: this.paciente.genero },
+    { label: 'Teléfono', value: this.paciente.telefono },
+    { label: 'Email', value: this.paciente.email },
+  ];
+
+  medicalData = [];
 
   onClose(): void {
     this.dialogRef.close();
