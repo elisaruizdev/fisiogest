8b50b63e2a1972a83b1be75b211e1f0af85ce581
Elisa Ruiz <elisaruizdev@gmail.com>
2025-11-10 12:55:55 +0100

    create component and open modal form

---
+++ src/app/features/private-area/patients/new-patient/new-patient.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { Modal } from '../../../../shared/ui/modal/modal';
+
+@Component({
+  selector: 'app-new-patient',
+  imports: [Modal],
+  templateUrl: './new-patient.html',
+  styleUrl: './new-patient.scss',
+})
+export class NewPatient {
+
+}
